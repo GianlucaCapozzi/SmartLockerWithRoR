11b96ce170996ddce715f1668c2b6a5d60f891b2
Gianluca Capozzi <capozzigianluca96@gmail.com>
2020-02-21 10:57:06 +0100

    Add home page

--- SmartLockerApp/app/src/main/java/com/iot/smartlockerapp/MainActivity.java
+++ SmartLockerApp/app/src/main/java/com/iot/smartlockerapp/MainActivity.java
@@ -1,19 +1,49 @@
 package com.iot.smartlockerapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     static String url = "http://10.0.2.2:3000";
 
+    private BottomNavigationView bottomNavigation;
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            String name = getIntent().getStringExtra("name");
+                            openFragment(HomeFragment.newInstance(name));
+                    }
+                    return false;
+                }
+            }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.navigation);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+    }
+
+    private void openFragment(Fragment fragment){
+
     }
 }
