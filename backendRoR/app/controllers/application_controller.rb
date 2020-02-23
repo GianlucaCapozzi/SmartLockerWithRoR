433c02fc04a5d58872f65f1b106412028dbd4b89
Gianmarco Cariggi <cariggi.1698481@studenti.uniroma1.it>
2020-02-23 18:39:04 +0100

    Completed login

--- backendRoR/app/controllers/application_controller.rb
+++ backendRoR/app/controllers/application_controller.rb
@@ -1,24 +1,11 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate_user!
+    before_action :authenticate_request
+    attr_reader :current_user
 
-    def render_resource(resource)
-        if resource.errors.empty?
-          render json: resource
-        else
-          validation_error(resource)
-        end
-    end
-    
-    def validation_error(resource)
-        render json: {
-            errors: [
-                {
-                status: '400',
-                title: 'Bad Request',
-                detail: resource.errors,
-                code: '100'
-                }
-            ]
-        }, status: :bad_request
+    private
+
+    def authenticate_request
+        @current_user = AuthorizeApiRequest.call(request.headers).result
+        render json: { error: 'Not Authorized' }, status: 401 unless @current_user
     end
 end
