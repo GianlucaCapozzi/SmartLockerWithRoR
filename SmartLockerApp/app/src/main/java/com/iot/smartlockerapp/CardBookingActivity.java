2515349c35fe43b7a6b940c4d1346b58cc1e2d23
Gianluca Capozzi <capozzigianluca96@gmail.com>
2020-02-24 09:40:06 +0100

    Add ActiveBooking

---
+++ SmartLockerApp/app/src/main/java/com/iot/smartlockerapp/CardBookingActivity.java
@@ -0,0 +1,308 @@
+package com.iot.smartlockerapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.fragment.app.FragmentActivity;
+
+public class CardBookingActivity extends AppCompatActivity {
+
+    private Button authenticate;
+    private Button leave;
+    private Button delete;
+
+    private TextView parkNameTV;
+    private TextView lockNameTV;
+
+    private String lockName;
+    private String lockHash;
+    private String parkName;
+    private String date;
+    private boolean lockState;
+
+    private final static String TAG = "FPrintAct";
+
+    // Firebase db
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        db = FirebaseFirestore.getInstance();
+
+        String user = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+
+        setContentView(R.layout.activity_booking_card);
+
+        parkNameTV = (TextView) findViewById(R.id.idParkName);
+        lockNameTV = (TextView) findViewById(R.id.idLockName);
+
+        parkName = getIntent().getStringExtra("park");
+        lockHash = getIntent().getStringExtra("lockHash");
+        lockName = getIntent().getStringExtra("lockName");
+        lockState = getIntent().getBooleanExtra("lockState", true);
+        date = getIntent().getStringExtra("date");
+
+        Log.d(TAG, "LockName: " + lockName);
+        Log.d(TAG, "LockState: " + lockState);
+
+        String lockN = lockName.substring(0, lockName.length()-1);
+        String lockID = lockName.substring(lockName.length()-1);
+
+        parkNameTV.setText(parkName);
+        lockNameTV.setText(lockN + " " + lockID);
+
+        authenticate = (Button) findViewById(R.id.idAuthButt);
+        leave = (Button) findViewById(R.id.idLeaveBtn);
+        delete = (Button) findViewById(R.id.idDeleteBtn);
+
+        final String bookID = user + " " + parkName + " " + date + " " + parkName + lockName;
+        Log.d(TAG, "BookID: " + bookID);
+
+        leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leaveLocker(bookID, parkName, lockHash);
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Confirm delete !");
+                builder.setMessage("You are about to delete your booking. Do you really want to proceed ?");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteBooking(bookID, parkName, lockHash);
+                        Toast.makeText(getApplicationContext(), "You've choosen to delete you booking", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        getApplicationContext().startActivity(i);
+                        finish();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "You've changed your mind", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        FragmentActivity activity = this;
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(activity, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    // user clicked negative button
+                } else {
+                    //TODO: Called when an unrecoverable error has been encountered and the operation is complete.
+                }
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                setLockFull(parkName, lockHash);
+
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //TODO: Called when a biometric is valid but not recognized.
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Put your finger over the sensor")
+                .setSubtitle("Set the subtitle to display.")
+                .setDescription("Set the description to display")
+                .setNegativeButtonText("Negative Button")
+                .build();
+
+        authenticate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+    }
+
+    private void setLockFull(String parkName, String lockHash){
+        Map<String, Object> lock = new HashMap<>();
+        if(lockState == true) {
+            lock.put("open", false);
+            lockState = false;
+        }
+        else{
+            lock.put("open", true);
+            lockState = true;
+        }
+
+        db.collection("parks/"+parkName.hashCode()+"/lockers").document(lockHash)
+                .set(lock, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    private void leaveLocker(String bookID, String parkName, String lockHash){
+        Map<String, Object> lock = new HashMap<>();
+        if(lockState == true) {
+            lock.put("open", false);
+        }
+        lock.put("available", true);
+        lock.put("user", "");
+        lockState = false;
+
+        db.collection("parks/"+parkName.hashCode()+"/lockers").document(lockHash)
+                .set(lock, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+        DateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+        Date currentTime = Calendar.getInstance().getTime();
+        String leaveTime = dateFormat.format(currentTime);
+        Map<String, Object> booking = new HashMap<>();
+        booking.put("active", false);
+        booking.put("leave", leaveTime);
+
+        db.collection("bookings").document(Integer.toString(bookID.hashCode()))
+                .set(booking, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+        Intent i = new Intent(this, MainActivity.class);
+        finish();
+    }
+
+    private void deleteBooking(String bookID, String parkName, String lockHash){
+
+        Map<String, Object> lock = new HashMap<>();
+        if(lockState == true) {
+            lock.put("open", false);
+        }
+        lock.put("available", true);
+        lock.put("user", "");
+        lockState = false;
+
+        db.collection("parks/"+parkName.hashCode()+"/lockers").document(lockHash)
+                .set(lock, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+        db.collection("bookings").document(Integer.toString(bookID.hashCode()))
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+
+    }
+
+}
--- SmartLockerApp/app/src/main/java/com/iot/smartlockerapp/HomeFragment.java
+++ SmartLockerApp/app/src/main/java/com/iot/smartlockerapp/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.iot.smartlockerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,24 +11,42 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
 
-    private TextView usernameTV;
+    private String TAG = "HOME";
 
-    private String name;
+    private TextView usernameTV;
+    private RecyclerView bookedRV;
+
+    private FirestoreRecyclerAdapter bookingAdapter;
+
+    private String user;
+    private String lockName;
+    private boolean lockState;
 
     private FirebaseFirestore db;
 
     public HomeFragment() {
     }
 
-    public static HomeFragment newInstance(String name) {
+    public static HomeFragment newInstance(String user) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString("name", name);
+        args.putString("user", user);
         fragment.setArguments(args);
         return fragment;
     }
@@ -36,7 +55,7 @@ public class HomeFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(getArguments() != null){
-            name = getArguments().getString("name");
+            user = getArguments().getString("user");
         }
     }
 
@@ -44,9 +63,91 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        Log.d("HOME", name);
+        Log.d(TAG, user);
+
+        db = FirebaseFirestore.getInstance();
+
         usernameTV = (TextView) v.findViewById(R.id.usernameView);
-        usernameTV.setText("Welcome back, " + name);
+        usernameTV.setText("Welcome back, " + user);
+
+        bookedRV = (RecyclerView) v.findViewById(R.id.bookedRV);
+        bookedRV.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        getUserActiveBookings();
+
         return v;
     }
+
+    private void getUserActiveBookings(){
+        Query query = db.collection("bookings")
+                .whereEqualTo("user", user)
+                .whereEqualTo("active", true);
+
+        FirestoreRecyclerOptions<Booking> response = new FirestoreRecyclerOptions.Builder<Booking>()
+                .setQuery(query, Booking.class)
+                .build();
+
+        Log.d(TAG, "In activeBookings");
+
+        bookingAdapter = new FirestoreRecyclerAdapter<Booking, BookingActiveHolder>(response) {
+
+            @NonNull
+            @Override
+            public BookingActiveHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.booking_card, parent, false);
+                return new BookingActiveHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull BookingActiveHolder bookingHolder, int i, @NonNull final Booking booking) {
+                getLockInfo(booking.getPark(), booking.getLockHash());
+                bookingHolder.parkB.setText(booking.getPark());
+                bookingHolder.dateB.setText(booking.getDate());
+                bookingHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(v.getContext(), CardBookingActivity.class);
+                        i.putExtra("park", booking.getPark());
+                        i.putExtra("date", booking.getDate());
+                        i.putExtra("lockHash", booking.getLockHash());
+                        i.putExtra("lockName", lockName);
+                        i.putExtra("lockState", lockState);
+                        v.getContext().startActivity(i);
+                    }
+                });
+            }
+        };
+        bookingAdapter.notifyDataSetChanged();
+        bookedRV.setAdapter(bookingAdapter);
+    }
+
+    public class BookingActiveHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.parkTV)
+        TextView parkB;
+        @BindView(R.id.dateTV)
+        TextView dateB;
+
+        public BookingActiveHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+
+    private void getLockInfo(String parkName, String lockHash){
+        DocumentReference docRef = db.collection("parks/"+parkName.hashCode()+"/lockers").document(lockHash);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Locker lock = documentSnapshot.toObject(Locker.class);
+                lockName = lock.getLockName();
+                Log.d(TAG, "LockName: " + lockName);
+                lockState = lock.isOpen();
+                Log.d(TAG, "LockState: " + lockState);
+            }
+        });
+    }
+
 }
