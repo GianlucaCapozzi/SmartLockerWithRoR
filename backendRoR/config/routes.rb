9d4cbfb8fc366c558854c9c0f4cabad422518be5
Gianluca Capozzi <capozzigianluca96@gmail.com>
2020-02-29 16:38:08 +0100

    Add sample user

--- backendRoR/config/routes.rb
+++ backendRoR/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'login', to: 'authentication#authenticate'
-  get 'logout', to: 'blacklisting#blacklist'
-  get 'activation/:id', to: 'registration#confirm_email'
+  post 'login', to: 'authentication#authenticate'
+  post 'logout', to: 'blacklisting#blacklist'
+  post 'activation/:id', to: 'registration#confirm_email'
 
   post 'signup', to: 'registration#registrate'
   post 'confsignup', to: 'configuration#configure'
